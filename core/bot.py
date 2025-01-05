c0a33689467e0e349242c7278d90f2c80f2ee176
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-01-05 16:11:11 +0000

    Added basic bot class and prefix handler

--- core/bot.py
+++ core/bot.py
@@ -1,2 +1,38 @@
 from discord.ext import commands
+from discord import Intents
+
+from config.settings import BOT_TOKEN
+from core.prefix import get_prefix
+
+class Bot(commands.Bot):
+
+    token: str
+
+    def __init__(self, **kwargs):
+        """
+        Initialising bot with state variables and setting required attributes
+        """
+        if not BOT_TOKEN:
+            raise ValueError('No bot token provided')
+        self.token = BOT_TOKEN
+        
+        super().__init__(
+            case_insensitive = True,
+            command_prefix = get_prefix,
+            heartbeat_timeout = 150.0,
+            intents = Intents.all(),
+            **kwargs,
+        )
+
+    def run(self, *args, **kwargs):
+        """
+        Run the bot
+        """
+        super().run(self.token, *args, **kwargs)
+    
+    async def on_ready(self):
+        """
+        On bot ready event
+        """
+        print(f'{self.user.name} is online! ')
 
---
+++ core/prefix.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+from discord import Message
+
+from config.settings import COMMAND_PREFIX
+
+
+async def get_prefix(bot: commands.Bot, message: Message):
+    """
+    Get the prefix for a message
+    """
+    prefix_set = [ COMMAND_PREFIX ]
+    if message.guild:
+        pass # To be implemented
+    return commands.when_mentioned_or(*prefix_set)(bot, message)
