0302b72666f8ba6f538d65b936404c5bb331338a
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-01-05 17:37:16 +0000

    Added paths and logging

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,14 @@
 from decouple import config
+from pathlib import Path
 
+# Base Settings
 COMMAND_PREFIX = config('DISCORD_COMMAND_PREFIX', default='$')
-
 BOT_TOKEN = config('DISCORD_BOT_TOKEN')
+
+# Paths
+BASE_DIR = Path(__file__).resolve().parent.parent
+COGS_DIR = BASE_DIR / 'cogs'
+LOGS_DIR = BASE_DIR / 'logs'
+
+# Logging
+LOG_FILE = LOGS_DIR / 'bot.log'
